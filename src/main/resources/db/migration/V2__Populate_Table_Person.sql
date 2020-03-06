d13ae273e590f1c4e242c0a841a458871b85035b
lordkure <LordKure1982>
2020-03-06 19:18:11 -0300

    Implementando Migration com flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,23 @@
+INSERT INTO `rest_spring_boot`.`person`
+(`address`,
+`first_name`,
+`gender`,
+`last_name`)
+VALUES
+('Rua Francisco Saracosa, 401',
+'Cleber',
+'Male',
+'Santos');
+SELECT * FROM rest_spring_boot.person;
+
+INSERT INTO `rest_spring_boot`.`person`
+(`address`,
+`first_name`,
+`gender`,
+`last_name`)
+VALUES
+('Rua Francisco Saracosa, 401',
+'Cintia',
+'Female',
+'Santos');
+SELECT * FROM rest_spring_boot.person;
