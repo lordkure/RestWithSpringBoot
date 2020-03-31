f4263df01e2f965df183d4dbda562a50abbb56eb
lordkure <LordKure1982>
2020-03-31 11:44:25 -0300

    Teste com banco de dados

--- src/test/java/br/com/cleber/restwithspringboot/RestWithSpringBootApplicationTests.java
+++ src/test/java/br/com/cleber/restwithspringboot/RestWithSpringBootApplicationTests.java
@@ -1,13 +1,29 @@
 package br.com.cleber.restwithspringboot;
 
+import br.com.cleber.restwithspringboot.data.vo.PersonVO;
+import com.github.dozermapper.core.inject.Inject;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class RestWithSpringBootApplicationTests {
 
+    @Autowired
+    private PersonController controller;
+
     @Test
-    void contextLoads() {
+    void teste() {
+        PersonVO vo = new PersonVO();
+        vo.setGender("Male");
+        vo.setFirstName("First");
+        vo.setLastName("Last");
+        vo.setAddress("Address");
+        PersonVO response = controller.create(vo).getBody();
+        Assert.assertEquals(response, controller.findById(response.getId()).getBody());
+        controller.delete(response.getId());
     }
 
 }
